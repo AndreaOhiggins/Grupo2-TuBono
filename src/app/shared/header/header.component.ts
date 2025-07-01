633dbf3dbf6fb0373390e1b63122834f1730af82
ayleensalcedo <163939180+ayleensalcedo@users.noreply.github.com>
2025-07-01 16:37:52 -0500

    Add header's sidenav

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 // import {MatIconModule} from '@angular/material/icon';
 // import {MatButtonModule} from '@angular/material/button';
 // import {MatToolbarModule} from '@angular/material/toolbar';
@@ -9,10 +10,17 @@ import { Component } from '@angular/core';
     // MatToolbarModule, 
     // MatButtonModule, 
     // MatIconModule
+     CommonModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  sidebarOpen = false;
+
+   toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 
 }
