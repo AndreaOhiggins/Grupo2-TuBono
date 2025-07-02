0e705ebd78dedfdd793cc868c73eb2185744d933
Andrea O'Higgins <u20221b178@upc.edu.pe>
2025-07-02 15:40:18 -0500

    feat: add profile service

---
+++ src/app/profile/services/profile.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, retry, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  base_URL = "http://localhost:8080/api/v1";
+
+  constructor(private http: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.log(`An error ocurred ${error.status}, body was: ${error.error}`);
+    } else {
+      console.log(`Backend returned code ${error.status}, body was: ${error.error}`);
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+
+  getUserById(id : any): Observable<any> {
+    return this.http.get<any>(`${this.base_URL}/user/${id}`, this.httpOptions).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
+  }
+
+  updateUser(id: any, user: any): Observable<any> {
+    return this.http.put<any>(`${this.base_URL}/user/${id}`, JSON.stringify(user), this.httpOptions).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
+  }
+
+  deleteUser(id: any): Observable<any> {
+    return this.http.delete<any>(`${this.base_URL}/user/${id}`, this.httpOptions).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
+  }
+
+
+}
