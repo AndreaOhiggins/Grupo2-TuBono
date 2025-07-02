2250cca472943d94a37442386471e2107ec2a903
Andrea O'Higgins <u20221b178@upc.edu.pe>
2025-07-02 15:47:40 -0500

    feat: add bond form logic

--- src/app/bond/components/bond-form/bond-form.component.ts
+++ src/app/bond/components/bond-form/bond-form.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BondService } from '../../services/bond.service';
 
 @Component({
   selector: 'app-bond-form',
@@ -6,6 +8,48 @@ import { Component } from '@angular/core';
   templateUrl: './bond-form.component.html',
   styleUrl: './bond-form.component.css'
 })
-export class BondFormComponent {
+export class BondFormComponent implements OnInit {
+
+  
+  isEditMode: boolean = false;
+  bondId: any;
+  bondData: any;
+  constructor(private route: ActivatedRoute, private bondService: BondService) {
+
+    // Check if the route contains a bondId parameter to determine if it's in edit mode
+    this.isEditMode = this.route.snapshot.paramMap.has('bondId');
+    console.log('Is Edit Mode:', this.isEditMode);
+    
+    // If in edit mode, get the bondId from the route parameters
+    if (this.isEditMode) {
+      this.route.paramMap.subscribe(params => {
+        this.bondId = params.get('bondId');
+        console.log('Bond ID:', this.bondId);
+      });
+    }
+  }
+
+
+  ngOnInit(): void {
+    // If in edit mode, fetch the bond data by ID
+    if (this.isEditMode && this.bondId) {
+      console.log('Fetching bond data for ID:', this.bondId);
+      this.getBondById();
+    }
+    
+  }
+
+  getBondById() {
+    this.bondService.getBondById(this.bondId).subscribe({
+      next: (response) => {
+        this.bondData = response;
+        console.log('Bond fetched successfully:', response);
+        // You can now use the response to populate your form fields
+      },
+      error: (error) => {
+        console.error('Error fetching bond:', error);
+      }
+    });
+  }
 
 }
